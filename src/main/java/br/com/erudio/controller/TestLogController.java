8ca3641021c4161a5d5789545ac5cb78572df0a1
Thyago Nunes <thyago.nunes@sigep.com.br>
2025-08-11 15:45:22 -0300

    Seção 11: Adicionando Suporte à Migrations com Flyway

--- src/main/java/br/com/erudio/controller/TestLogController.java
+++ src/main/java/br/com/erudio/controller/TestLogController.java
@@ -3,14 +3,16 @@ package br.com.erudio.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test/v1")
 public class TestLogController {
 
     private final Logger logger = LoggerFactory.getLogger(TestLogController.class);
 
-    @GetMapping("/test")
+    @GetMapping
     public String testLog() {
         logger.debug("This is a debug message");
         logger.info("This is a info message");
